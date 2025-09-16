4b8e5201cc5dc1ab2ba0b734c7f7af23cbd31809
UnkowwnPhysics <davidferreiragithub@gmail.com>
2025-09-16 19:08:18 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,      // força a porta 3000
-    strictPort: true // se 3000 estiver ocupada, dá erro ao invés de escolher outra porta
+  preview: {
+    allowedHosts: ['calculator-b9q5.onrender.com'],
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
   }
-});
+})
+
 
